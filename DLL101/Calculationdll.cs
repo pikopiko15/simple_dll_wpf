9d6700f6dbc8b3946b80d2c057e60da692a14c7c
Maralov Kemal <kemo181098@gmail.com>
2020-09-21 01:18:03 +0600

    Add project files.

---
+++ DLL101/Calculationdll.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL101
+{
+    public class Calculationdll
+    {
+        public int Add(int a, int b) => a + b;
+        public int Sub(int a, int b) => a - b;
+
+        
+    }
+}
---
+++ UsingDLL/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DLL101;
+
+namespace UsingDLL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Calculationdll calc = new Calculationdll();
+
+        private void BtnAdd(object sender, EventArgs e)
+        {
+            try
+            {
+                // storing the result in int r
+                int r = calc.Add(int.Parse(txtBoxForFirstNumber.Text), int.Parse(txtBoxForSecondNumber.Text));
+                txtBoxResult.Text = r.ToString();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnSub(object sender, EventArgs e)
+        {
+            try
+            {
+                // storing the result in int r
+                int r = calc.Sub(int.Parse(txtBoxForFirstNumber.Text), int.Parse(txtBoxForSecondNumber.Text));
+                txtBoxResult.Text = r.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
